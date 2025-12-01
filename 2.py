1b8af30e6d5220dcc69e5e7db360e682c13c9a8e
Vovunchik-Kavunchik <safvladvic5@gmail.com>
2025-12-01 20:17:02 +1000

    добавлены ошибки

--- 2.py
+++ 2.py
@@ -1,7 +1,21 @@
 import cv2
+from PIL import Image
+while True:
+    try:
+        x=input ("введите название изображения")
+        im = Image.open("Images/"+x+".jpg")
+        im.show()
+        break
+    except Exception as e:
+        print("Попробуй снова ошибка ", e)
+while True:
+        #for x in range(0:im.size):
+        y=input ('введи новое имя файла')
+        try:
+            im.save("Images/" + y + ".jpg")
+            im.show()
+            break
+        except Exception as d:
+            print("Попробуй снова ошибка ", d)
 
-x=input ("введите название изображения")
-image = cv2.imread("Images/"+x+".jpg")
-cv2.imshow("Slicend image",image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
